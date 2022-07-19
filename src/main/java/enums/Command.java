97d660772f437b763cc9196a569b885a35ab0eb3
Diana Tretyakova <diana.r.tretyakova@gmail.com>
2022-07-19 08:22:29 +0500

    refactoring Command class

--- src/main/java/enums/Command.java
+++ src/main/java/enums/Command.java
@@ -2,16 +2,4 @@ package enums;
 
 public enum Command {
     ENCRYPT, DECRYPT, BRUTE_FORCE, EXIT
-//    ENCRYPT("1"), DECRYPT("2"), BRUTE_FORCE("3"), EXIT("4");
-//
-//    private final String number;
-//
-//    Command(final String number) {
-//        this.number = number;
-//    }
-//
-//    @Override
-//    public String toString() {
-//        return "Command number: " + number;
-//    }
 }
